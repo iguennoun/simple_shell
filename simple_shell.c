b45184739a7eb1b93026c488679244dcca5d8ba8
GUENNOUN Ismail <ismail.guennoun@yahoo.fr>
2023-05-24 17:51:27 -0700

    Checking betty style

--- simple_shell.c
+++ simple_shell.c
@@ -14,23 +14,23 @@
  */
 char **tokensInLine(char *str)
 {
-        char **params = NULL, *param, delimiters[] = " \n\t";
-        unsigned int i = 0;
+	char **params = NULL, *param, delimiters[] = " \n\t";
+	unsigned int i = 0;
 
-        param = strtok(str, delimiters);
-        while (param)
-        {
-                i++;
-                params = realloc(params, i * sizeof(char *));
-                if (!params)
-                {
-                        exit(EXIT_FAILURE);
-                }
-                params[i - 1] = param;
-                param = strtok(NULL, delimiters);
-        }
-        params[i] = NULL;
-        return (params);
+	param = strtok(str, delimiters);
+	while (param)
+	{
+		i++;
+		params = realloc(params, i * sizeof(char *));
+		if (!params)
+		{
+			exit(EXIT_FAILURE);
+		}
+		params[i - 1] = param;
+		param = strtok(NULL, delimiters);
+	}
+	params[i] = NULL;
+	return (params);
 }
 
 /**
@@ -42,18 +42,18 @@ char **tokensInLine(char *str)
  */
 char *getEnvVarVal(const char *name, char **env)
 {
-        char **environ = env;
-        int i = 0;
-        char *envvkey;
+	char **environ = env;
+	int i = 0;
+	char *envvkey;
 
-        while (environ[i])
-        {
-                envvkey = strtok(environ[i], "=");
-                if (strcmp(envvkey, name) == 0)
-                        return (strtok(NULL, "="));
-                i++;
-        }
-        return (NULL);
+	while (environ[i])
+	{
+		envvkey = strtok(environ[i], "=");
+		if (strcmp(envvkey, name) == 0)
+			return (strtok(NULL, "="));
+		i++;
+	}
+	return (NULL);
 }
 
 /**
@@ -65,26 +65,26 @@ char *getEnvVarVal(const char *name, char **env)
  */
 char *checkCommand(char *command, char **env)
 {
-        char *path_dirs = getEnvVarVal("PATH", env);
-        char *path_dir;
-        char *cmd_full_path;
-        struct stat st;
+	char *path_dirs = getEnvVarVal("PATH", env);
+	char *path_dir;
+	char *cmd_full_path;
+	struct stat st;
 
-        path_dir = strtok(path_dirs, ":");
-        while (path_dir)
-        {
-                cmd_full_path = malloc(strlen(path_dir) + strlen(command) + 2);
-                strcpy(cmd_full_path, path_dir);
-                strcat(cmd_full_path, "/");
-                strcat(cmd_full_path, command);
-               
-	       	if (stat(cmd_full_path, &st) == 0)
-                        return (cmd_full_path);
-               
-	       	free(cmd_full_path);
-                path_dir = strtok(NULL, ":");
-        }
-        return (NULL);
+	path_dir = strtok(path_dirs, ":");
+	while (path_dir)
+	{
+		cmd_full_path = malloc(strlen(path_dir) + strlen(command) + 2);
+		strcpy(cmd_full_path, path_dir);
+		strcat(cmd_full_path, "/");
+		strcat(cmd_full_path, command);
+
+		if (stat(cmd_full_path, &st) == 0)
+			return (cmd_full_path);
+
+		free(cmd_full_path);
+		path_dir = strtok(NULL, ":");
+	}
+	return (NULL);
 }
 
 /**
@@ -97,45 +97,44 @@ char *checkCommand(char *command, char **env)
  */
 int main(int ac, char **av, char **env)
 {
-        char *shell_name = "($) ", *shell_line = NULL, *cmd = NULL;
-        size_t linecap = 0;
-        ssize_t linelen;
-        char **tokens = NULL;
-        pid_t pid;
-        int status;
+	char *shell_name = "($) ", *shell_line = NULL, *cmd = NULL;
+	size_t linecap = 0;
+	ssize_t linelen;
+	char **tokens = NULL;
+	pid_t pid;
+	int status;
 
-        (void) ac, (void) av;
-        while (1)
-        {
-                printf("%s", shell_name);
-                linelen = getline(&shell_line, &linecap, stdin);
-                if (linelen == EOF)
-                {
-                        exit(EXIT_FAILURE);
-                }
-                shell_line[linelen - 1] = '\0';
+	(void) ac, (void) av;
+	while (1)
+	{
+		printf("%s", shell_name);
+		linelen = getline(&shell_line, &linecap, stdin);
+		if (linelen == EOF)
+		{
+			exit(EXIT_FAILURE);
+		}
+		shell_line[linelen - 1] = '\0';
 
-                /* tokens = malloc(sizeof(char *) * 1024); */
-                tokens = tokensInLine(shell_line);
-                /* printf("%s\n", shell_line); */
-                if (strcmp(tokens[0], "exit") == 0)
-                        exit(EXIT_SUCCESS);
-
-                pid = fork();
-                if (pid == 0)
-                {
-                        cmd = checkCommand(tokens[0], env);
-                        if (cmd)
-                                execve(cmd, tokens, env);
-                        else
-                                printf("%s: No such file or directory\n", av[0]);
+		/* tokens = malloc(sizeof(char *) * 1024); */
+		tokens = tokensInLine(shell_line);
+		/* printf("%s\n", shell_line); */
+		if (strcmp(tokens[0], "exit") == 0)
 			exit(EXIT_SUCCESS);
-                }
-                else
-                        wait(&status);
-                /* free(shell_line);*/
-                free(tokens);
-        }
-        exit(EXIT_SUCCESS);
 
+		pid = fork();
+		if (pid == 0)
+		{
+			cmd = checkCommand(tokens[0], env);
+			if (cmd)
+				execve(cmd, tokens, env);
+			else
+				printf("%s: No such file or directory\n", av[0]);
+			exit(EXIT_SUCCESS);
+		}
+		else
+			wait(&status);
+		/* free(shell_line);*/
+		free(tokens);
+	}
+	exit(EXIT_SUCCESS);
 }
